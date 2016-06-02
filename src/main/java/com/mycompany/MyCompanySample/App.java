5e27d368ab87b77e4897f8add8864800fdea193a
Kartheeka <kartheeka_pedda_gondi@fanniemae.com>
2016-06-02 15:08:41 -0400

    broke the code 2nd time

--- src/main/java/com/mycompany/MyCompanySample/App.java
+++ src/main/java/com/mycompany/MyCompanySample/App.java
@@ -6,7 +6,7 @@ package com.mycompany.MyCompanySample;
  */
 public class App 
 {
-    public static void main( String[] args )
+    pc static void main( String[] args )
     {
         System.out.println( "Hello World! );
     }
